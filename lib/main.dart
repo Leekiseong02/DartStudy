eedd51f17b7a4ee8b4c1fdca2527730e43bbd2bf
Leekiseong02 <leekisung0212@gmail.com>
2021-08-04 12:30:55 +0900

    Adding Layout Widgets
    Column
    -> RaisedButton (deprecated widget)

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Text('This is my default text!'),
-      appBar: AppBar(
-        title: Text('My First App'),
+      home: Scaffold(
+        body: Column(
+          children: [
+            Text('Hello World!1'),
+            Text('Hello World!2'),
+            RaisedButton(child: Text('Button 1'), onPressed: null),
+            RaisedButton(child: Text('Button 2'), onPressed: null),
+            RaisedButton(child: Text('Button 3'), onPressed: null),
+          ],
+        ),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
       ),
-    ));
+    );
   }
 }
