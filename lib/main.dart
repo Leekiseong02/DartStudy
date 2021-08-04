2e10c9e467797667439805231f0ee6a9ad48ca27
Leekiseong02 <leekisung0212@gmail.com>
2021-08-04 13:27:37 +0900

    Connecting Function & Buttons
    Use function

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Press the button');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,9 +18,9 @@ class MyApp extends StatelessWidget {
           children: [
             Text('Hello World!1'),
             Text('Hello World!2'),
-            RaisedButton(child: Text('Button 1'), onPressed: null),
-            RaisedButton(child: Text('Button 2'), onPressed: null),
-            RaisedButton(child: Text('Button 3'), onPressed: null),
+            RaisedButton(child: Text('Button 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Button 2'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Button 3'), onPressed: answerQuestion),
           ],
         ),
         appBar: AppBar(
